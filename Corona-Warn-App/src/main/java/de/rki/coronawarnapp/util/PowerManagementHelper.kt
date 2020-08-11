a97aad6bd3ed70c0d8d39e7db62f16b8c15710eb
Oliver Zimmerman <oezimmerman@gmail.com>
2020-08-11 14:35:22 +0100

    Added method to check if locationless scanning is supported, an Android 11 feature

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/PowerManagementHelper.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/PowerManagementHelper.kt
@@ -1,6 +1,7 @@
 package de.rki.coronawarnapp.util
 
 import android.content.Context
+import android.os.Build
 import android.os.PowerManager
 
 object PowerManagementHelper {
@@ -10,5 +11,12 @@ object PowerManagementHelper {
     fun isIgnoringBatteryOptimizations(context: Context): Boolean {
         val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
         return powerManager.isIgnoringBatteryOptimizations(context.packageName)
+    }
+
+    /**
+     *  Checks if device supports locationless scanning.
+     */
+    fun deviceSupportsLocationlessScanning(context: Context): Boolean {
+        return Build.VERSION.SDK_INT > Build.VERSION_CODES.Q
     }
 }
