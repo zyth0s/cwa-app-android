f55cfd703fa4a514ddcdef31794bb841e296cb71
BMitter <Berndus@gmx.de>
2020-08-27 11:28:49 +0200

    EXPOSUREAPP-2289 POC (WIP)
    Move ExposureStateUpdateWorker to foreground
    - Testable Version (under evaluation)

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/nearby/ExposureStateUpdateWorker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/nearby/ExposureStateUpdateWorker.kt
@@ -5,22 +5,27 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.nearby.exposurenotification.ExposureNotificationClient
+import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.exception.ExceptionCategory
 import de.rki.coronawarnapp.exception.NoTokenException
 import de.rki.coronawarnapp.exception.TransactionException
 import de.rki.coronawarnapp.exception.reporting.report
 import de.rki.coronawarnapp.storage.ExposureSummaryRepository
 import de.rki.coronawarnapp.transaction.RiskLevelTransaction
+import de.rki.coronawarnapp.worker.BackgroundWorkHelper
 import timber.log.Timber
 
 class ExposureStateUpdateWorker(val context: Context, workerParams: WorkerParameters) :
     CoroutineWorker(context, workerParams) {
     companion object {
         private val TAG = ExposureStateUpdateWorker::class.simpleName
+        private val notificationID = TAG.hashCode()
     }
 
     override suspend fun doWork(): Result {
         try {
+            BackgroundWorkHelper.moveCoroutineWorkerToForeground(context.getString(R.string.notification_headline), "", notificationID, this)
+
             Timber.v("worker to persist exposure summary started")
             val token = inputData.getString(ExposureNotificationClient.EXTRA_TOKEN)
                 ?: throw NoTokenException(IllegalArgumentException("no token was found in the intent"))
